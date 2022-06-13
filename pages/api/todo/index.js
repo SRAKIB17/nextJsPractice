67a42d020234246e32758e189e1cdf960f12e7a3
SRAKIB17 <rakibulssc5@gmail.com>
2022-06-13 20:47:06 +0600

    don

--- pages/api/todo/index.js
+++ pages/api/todo/index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 export default async function handler(req, res) {
     const id = req.query;
-    const uri = `mongodb+srv://todoApp:25pom6z9r8cgZPsP@cluster0.v6ue1.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v6ue1.mongodb.net/?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     const run = async () => {
         await client.connect()
